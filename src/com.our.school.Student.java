39a71bc9d5de1d9daec5192ba406cb32f4b1b249
Rakesh Prabhakaran <rakesh.1985march@gmail.com>
2020-05-28 11:28:54 +1000

    Create com.our.school.Student.java

---
+++ src/com.our.school.Student.java
@@ -0,0 +1,14 @@
+package com.our.school;
+
+public class Student extends Person {
+	long totalMarks;
+
+	public long getTotalMarks() {
+		return totalMarks;
+	}
+
+	public void setTotalMarks(long totalMarks) {
+		this.totalMarks = totalMarks;
+	}
+	
+}

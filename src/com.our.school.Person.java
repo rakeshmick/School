e7f2b507dcc0450097854d753c11cad1960a10b4
Rakesh Prabhakaran <rakesh.1985march@gmail.com>
2020-05-28 11:21:28 +1000

    Create com.our.school.Person.java

---
+++ src/com.our.school.Person.java
@@ -0,0 +1,34 @@
+package com.our.school;
+
+public class Person {
+	String first_name;
+	String last_name;
+	long DoB;
+	String address;
+	
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public long getDoB() {
+		return DoB;
+	}
+	public void setDoB(long doB) {
+		DoB = doB;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}

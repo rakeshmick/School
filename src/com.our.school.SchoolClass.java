875e287c9d4e3f641e207098253a8129dc14ce57
Rakesh Prabhakaran <rakesh.1985march@gmail.com>
2020-05-28 11:27:30 +1000

    Create com.our.school.SchoolClass.java

---
+++ src/com.our.school.SchoolClass.java
@@ -0,0 +1,24 @@
+package com.our.school;
+
+import java.util.List;
+
+public class SchoolClass {
+	
+	Teacher teacher;
+	List<Student> student;
+	public Teacher getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+	public List<Student> getStudent() {
+		return student;
+	}
+	public void setStudent(List<Student> student) {
+		this.student = student;
+	}
+	
+	
+
+}

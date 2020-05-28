f36257b90719573ebea8ede9fff7edbd4b2cb153
Rakesh Prabhakaran <rakesh.1985march@gmail.com>
2020-05-28 11:29:45 +1000

    Create com.our.school.Teacher.Teacher.java

---
+++ src/com.our.school.Teacher.Teacher.java
@@ -0,0 +1,13 @@
+package com.our.school;
+
+public class Teacher extends Person{
+    long salary;
+
+	public long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+}
